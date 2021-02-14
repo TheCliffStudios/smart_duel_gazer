0a03ebb78a3f5111b53b9bb8da90317c44c775e7
SubtleDragon057 <devan_ninjapc@yahoo.ca>
2021-02-14 11:23:18 -0700

    Add files via upload

---
+++ Assets/NetworkInfo.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Network Info")]
+public class NetworkInfo : ScriptableObject
+{
+    public string userAddedIP;
+    public string userAddedPort;
+}
